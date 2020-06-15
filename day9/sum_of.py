68b64290b3b6caf076016e14940868abb2ab071d
balaji <balajikothinti26@gmail.com>
2020-06-15 13:22:15 +0530

    Day 9|Sum_of elements in array

--- day9/sum_of.py
+++ day9/sum_of.py
@@ -7,8 +7,20 @@ def sum_of(arr, first_el):
     return new
 
 
+def reverse_sum_of(arr):
+    last_el = arr[len(arr) - 1]
+    new = [last_el]
+    el = 0
+    for i in range(len(arr) - 1, 0, -1):
+        el = arr[i - 1] + new[0]
+        new.insert(0, el)
+    return new
+
+
 if __name__ == "__main__":
     inp = [1, 2, 3, 4]
     first_el = inp[0]
     out = sum_of(inp, first_el)
+    out_of_reverse_sum = reverse_sum_of(inp)
     print(out)
+    print(out_of_reverse_sum)
